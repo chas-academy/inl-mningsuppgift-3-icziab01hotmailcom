baf5b7d58adf95f0ba05a93e91b48aa3e53cc011
icziab_01@hotmail.com <icziab_01@hotmail.com>
2026-05-19 20:46:43 +0200

    Klar

--- src/script.js
+++ src/script.js
@@ -1,40 +1,43 @@
-// array för varukorgen
+
 let cart = [];
 
-// Hämta HTML-element
 const productInput = document.getElementById("productInput");
 const priceInput = document.getElementById("priceInput");
-const addBtn = document.getElementById("addBtn");
+const addButton = document.getElementById("addButton");
 const cartList = document.getElementById("cartList");
 
-// Button
-    function addToCart(){
+function addToCart(){
+    const productName = productInput.value
+    const productPrice = Number(priceInput.value)
 
-//hämta värden
-const productName = productInput.value;
-const productPrice = Number(priceInput.value);
+    const newProduct = {
+        productName: productName,
+        productPrice: productPrice,
+        quantity: 1,
+    }
 
-//Kontrollera
-let found = false;
+    let itemExist = false;
 
-    for(let product of cart){
-        if(product.productName === productName){
-            product.quantity++;
-            found = true;
+    for (item of cart){
+        if (item.productName === productName){
+            item.quantity++
+            itemExist = true;
         }
     }
-// Om produkten inte finns
-    if (!found){
-        cart.push({productName: productName, price: productPrice, quantity: 1});
+
+    if(!itemExist){
+        cart.push(newProduct)
     }
-//Visa varukorg
-    cartList.innerHTML = "";
-    for (let product of cart){
-        const li = document.createElement("li");
-        li.innerHTML = `${product.productName} - ${product.price} kr - Antal: ${product.quantity}`;
-        cartList.appendChild(li);
-    }
-//Rensa input
+    
     productInput.value = "";
     priceInput.value = "";
 }
+
+function renderCart(){
+    for (item of cart){
+        const li = document.createElement("li");
+        li.textContent = `${item.productName} - ${item.productPrice}kr (x${item.quantity})`
+        cartList.appendChild(li);
+    }
+}
+
